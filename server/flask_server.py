511a114fa4266637c8eb036d3062e286e2e3bd8e
psolanke <pratiksolankes@gmail.com>
2018-11-01 14:12:19 +0530

    Test Index Message

--- server/flask_server.py
+++ server/flask_server.py
@@ -2,9 +2,13 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Hello, World"
+
 @app.route('/api/v1.0/get_public_ip', methods=['GET'])
 def get_public_ip():
-	return jsonify({'ip': request.remote_addr}), 200
+    return jsonify({'ip': request.remote_addr}), 200
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
