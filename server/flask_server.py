1a4669ab4e95dc92f457a5874eeec188c452b36d
psolanke <pratiksolankes@gmail.com>
2018-11-01 16:13:39 +0530

    Update registry api

--- server/flask_server.py
+++ server/flask_server.py
@@ -10,5 +10,11 @@ def index():
 def get_public_ip():
     return jsonify({'ip': request.remote_addr}), 200
 
+@app.route('/api/v1.0/update_registry', methods=['POST'])
+def update_registry():
+    public_ip = request.remote_addr
+    mac_address = request.get_json()
+    return jsonify(mac_address), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
